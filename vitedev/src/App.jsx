ff1d9f7d20290be16bde91becb6ed6e14ae9aa4a
Albert S <albertseptiawan@ayoconnect.id>
2024-07-14 22:31:14 +0700

    init: vitedev inside folder

---
+++ vitedev/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center text-2xl mt-10">
+      Halo, Albert
+    </div>
+  )
+}
+
+export default App
---
+++ vitedev/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve('index.html'),
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    }
+  }
+})
