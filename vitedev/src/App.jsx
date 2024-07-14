6373c0c71ff5204be945ace9e2c8369f4934b3e8
Albert S <albertseptiawan@ayoconnect.id>
2024-07-14 22:36:49 +0700

    fix: setup (dist files)

--- vitedev/src/App.jsx
+++ vitedev/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import vitaLogo from '../public/vite.svg'
 
 function App() {
   return (
-    <div className="text-center text-2xl mt-10">
-      Halo, Albert
+    <div className="flex flex-col items-center gap-5 mt-10">
+      <img src={vitaLogo} alt="Vite Logo" />
+      <div className="text-center text-2xl">
+        Halo, Albert
+      </div>
+
     </div>
   )
 }
